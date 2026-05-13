15455419cc2a98ec3fef218d3587c9155460ffcd
Aralbekov Umar <umarmirlanbek@gmail.com>
2026-05-13 17:26:44 +0600

    ._.

--- insta_site/insta_site/asgi.py
+++ insta_site/insta_site/asgi.py
@@ -18,7 +18,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insta_site.settings')
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application()
+        "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(
             URLRouter(
                 websocket_urlpatterns
